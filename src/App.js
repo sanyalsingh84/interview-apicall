90ce9174bf11b36c75ea6cb5848485476cd1992f
Sanyal Singh <sanyalsingh84@gmail.com>
2021-08-05 00:50:28 +0530

    added loading feature

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ const App = () => {
   const [users, setUsers] = useState([]);
   const [totalPages, setTotalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       try {
         const { data } = await axios.get(
           `https://reqres.in/api/users?page=${currentPage}`
@@ -20,9 +22,11 @@ const App = () => {
           }
           setTotalPages(temp);
           setUsers(data.data);
+          setIsLoading(false);
         }
       } catch (error) {
         console.log(error);
+        setIsLoading(false);
       }
     };
     fetchData();
@@ -35,24 +39,32 @@ const App = () => {
   // console.log(users);
   return (
     <>
-      <div className="container">
-        {users &&
-          users.map((user) => {
-            return <Users key={user.id} user={user} />;
-          })}
-      </div>
-      <div className="pages">
-        <p>Pages:</p>
-        {totalPages &&
-          totalPages.map((page, i) => (
-            <button
-              onClick={() => handleClick(page)}
-              key={i}
-              className={currentPage === page ? "btn btn-active" : "btn"}>
-              {page}
-            </button>
-          ))}
-      </div>
+      {isLoading ? (
+        <div className="loading">
+          <h1>Loadding</h1>
+        </div>
+      ) : (
+        <>
+          <div className="container">
+            {users &&
+              users.map((user) => {
+                return <Users key={user.id} user={user} />;
+              })}
+          </div>
+          <div className="pages">
+            <p>Pages:</p>
+            {totalPages &&
+              totalPages.map((page, i) => (
+                <button
+                  onClick={() => handleClick(page)}
+                  key={i}
+                  className={currentPage === page ? "btn btn-active" : "btn"}>
+                  {page}
+                </button>
+              ))}
+          </div>
+        </>
+      )}
     </>
   );
 };
